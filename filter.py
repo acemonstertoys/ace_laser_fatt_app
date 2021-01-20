c43c65f33a94918f74161769c2311abb1cc0ee4b
Hal Rodriguez <halr66@gmail.com>
2021-01-20 12:35:03 -0800

    First pass at the filter data

---
+++ filter.py
@@ -0,0 +1,54 @@
+from enum import Enum
+import requests
+
+# Global Variables ----
+class FilterType(Enum):
+    GREEN_ORGANICS = 0
+    WHITE_SYNTHETICS = 1
+
+class Filter:
+    """
+    How to change the filter before the blower
+    https://wiki.acemakerspace.org/how-to-change-the-filter-before-the-blower/
+    """
+
+    def __init__(self, filterId, filterType, odometerReading):
+        self.filterId = filterId
+        self.filterType = filterType
+        self.startOdometer = odometerReading    # what units is the odometer in?
+        self.endOdometer = odometerReading
+
+    def createNewFiler(self):
+        """
+        docstring
+        """
+        # call GC
+        # return new filterID
+        pass
+
+    def changeFilter(self, oldFilterID, newFilterID):
+        """
+        docstring
+        """
+        self.updateRuntime()
+        pass
+
+    def updateRuntime(self, usageTime):
+        """
+        docstring
+        """
+        # call GC
+        pass
+
+    def calcRemainingTime(self):
+        """
+        Green organics filters can be used for a total of 140 minutes.
+        White synthetics filter can be used for a total of 60 minutes.
+        """
+        currentUsage = self.endOdometer - self.startOdometer
+        totalUsage = self.recordedUsage + currentUsage
+        if self.filterType == FilterType.GREEN_ORGANICS:
+            remainingTime = 140 - totalUsage
+        elif self.filterType == FilterType.WHITE_SYNTHETICS
+            remainingTime = 60 - totalUsage
+        return remainingTime
