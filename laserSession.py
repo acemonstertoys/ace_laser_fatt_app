da2e739b3579d268c4519aee5fa76a15b72e2b5b
Hal Rodriguez <halr66@gmail.com>
2021-02-27 15:50:20 -0800

    init from json dict

--- laserSession.py
+++ laserSession.py
@@ -1,17 +1,17 @@
 class LaserSession:
-    def __init__(self, credential, firstName='User', startingOdometer=0):
-        self.credential = credential
-        self.firstName = firstName
+    def __init__(self, userDict, startingOdometer=0):
+        self.credential = userDict['RFID']
+        self.name = userDict['First Name'] + ' ' + userDict['Last Initial']
         self.startingOdometer = startingOdometer
         self.currentOdometer = startingOdometer
 
-    def calculate_firing_time():
+    def calculate_firing_time(this):
         """
         docstring
         """
         return self.currentOdometer - self.startingOdometer
     
-    def calculate_session_cost():
+    def calculate_session_cost(this):
         """
         docstring
         """
