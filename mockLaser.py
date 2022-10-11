bf631a90ec8e2d99045b621068adfd08b185a5d3
Hal Rodriguez <halr66@gmail.com>
2022-10-11 13:58:30 -0700

    Added documentation and a debug line.

--- mockLaser.py
+++ mockLaser.py
@@ -1,6 +1,11 @@
 
 class MockLaser:
+    '''
+    MockLaser used in place of Laser for development purposes only.
+    SessionManager will instantiate MockLaser if the environment vairable LASERGUI_MOCK exists. 
+    '''
     def __init__(self, port='/dev/ttyACM0', baudrate=9600):
+        print("Initializing MockLaser...")
         self.port = port
         self.baudrate= baudrate
         self.enabled = False
@@ -14,4 +19,3 @@ class MockLaser:
 
     def status(self):
         self.odometer = '0'
-
